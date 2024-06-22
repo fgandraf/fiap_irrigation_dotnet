c9a5767b15110472816ea1957d44f52a243e8549
fgandraf <fgandraf@gmail.com>
2024-06-22 15:35:37 -0300

    feat: Add Data Context

--- IrrigationApi/Irrigation.Infra/Data/IrrigationDataContext.cs
+++ IrrigationApi/Irrigation.Infra/Data/IrrigationDataContext.cs
@@ -6,16 +6,28 @@ namespace Irrigation.Infra.Data;
 
 public class IrrigationDataContext: DbContext
 {
-    public DbSet<User> Users { get; set; } = null!;
+    
+    public DbSet<Area> Areas { get; set; } = null!;
+    public DbSet<Notification> Notifications { get; set; } = null!;
     public DbSet<Role> Roles { get; set; } = null!;
+    public DbSet<Schedule> Schedules { get; set; } = null!;
+    public DbSet<Sensor> Sensors { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Weather> Weathers { get; set; } = null!;
+    
 
     public IrrigationDataContext() { }
     public IrrigationDataContext(DbContextOptions<IrrigationDataContext> options) : base(options) { }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new UserMap());
+        modelBuilder.ApplyConfiguration(new AreaMap());
+        modelBuilder.ApplyConfiguration(new NotificationMap());
         modelBuilder.ApplyConfiguration(new RoleMap());
+        modelBuilder.ApplyConfiguration(new ScheduleMap());
+        modelBuilder.ApplyConfiguration(new SensorMap());
+        modelBuilder.ApplyConfiguration(new UserMap());
+        modelBuilder.ApplyConfiguration(new WeatherMap());
         base.OnModelCreating(modelBuilder);
     }
 }
